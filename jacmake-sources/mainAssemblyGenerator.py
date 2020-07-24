cd07df6705f8416e5b5ef6d9a3e59c79e79abf6a
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-07-24 10:15:50 +0200

    added support for linux

--- jacmake-sources/mainAssemblyGenerator.py
+++ jacmake-sources/mainAssemblyGenerator.py
@@ -1,9 +1,19 @@
+from platform import system as sys
+
+
 def generateMainAssembly(files_to_be_executed):
-    bare_file = open("/usr/local/share/jaclang-data/entry-macho.s")
+    if sys() == "Linux":
+        osplatform = "linux"
+    elif sys() == "Darwin":
+        osplatform = "OSX"
+    else:
+        print("Unsupported platform!")
+        exit(1)
+
+    bare_file = open(f"/usr/local/share/jaclang-data/entry-{'gnu' if osplatform == 'Linux' else 'macho'}.s")
     entry_code = bare_file.read().split("\n")
     main_index = entry_code.index("_main:") + 1
     for file in reversed(files_to_be_executed):
         file = file.replace("/", "..")
         entry_code.insert(main_index, f"   call s{file}")
     return "\n".join(entry_code)
-
