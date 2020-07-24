6c634547e4a38d5122863b1652928d19aea72f54
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-07-24 10:53:45 +0200

    removed warnings

--- jacmake-sources/main.py
+++ jacmake-sources/main.py
@@ -2,10 +2,18 @@ from sys import argv
 from os import system, remove
 from mainAssemblyGenerator import generateMainAssembly
 
-version = "1.0.2"
+version = "1.0.3"
 
 arguments = argv[1:]
 object_names = []
+
+if sys() == "Linux":
+        osplatform = "linux"
+    elif sys() == "Darwin":
+        osplatform = "OSX"
+    else:
+        print("Unsupported platform!")
+        exit(1)
 
 if not arguments:
     print(f"JACMAKE {version} - help")
@@ -19,7 +27,7 @@ for file in arguments:
     file_name = ".".join(file_name)
 
     system(f"jaclang {file} -o{file_name}.s --quiet")
-    system(f"gcc -s -c {file_name}.s -o{file_name}.o -no-pie")
+    system(f"gcc -c {file_name}.s -o{file_name}.o {'-no-pie' if osplatform == 'linux' else ''}")
     remove(f"{file_name}.s")
     object_names.append(f"{file_name}.o")
 
@@ -29,13 +37,13 @@ entry_file_path = entry_file_path_prefix + ("/" if entry_file_path_prefix else "
 entry_file = open(entry_file_path, "w")
 entry_file.write(generateMainAssembly([arguments[0]]))
 entry_file.close()
-system(f"gcc -c {entry_file_path} -o{entry_file_path[:-2]}.o -no-pie")
+system(f"gcc -c {entry_file_path} -o{entry_file_path[:-2]}.o {'-no-pie' if osplatform == 'linux' else ''}")
 remove(entry_file_path)
 
 object_names.append(f"{entry_file_path[:-2]}.o")
 
 print("Linking files")
-system(f"gcc {' '.join(object_names)} -o{object_names[0][:-2]} -no-pie")
+system(f"gcc {' '.join(object_names)} -o{object_names[0][:-2]} {'-no-pie' if osplatform == 'linux' else ''}")
 
 for object_file in object_names:
     remove(object_file)
