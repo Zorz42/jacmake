31d3e428e76a0b311079e64401fad2a5e6dbbc9b
Zorz42 <54270248+Zorz42@users.noreply.github.com>
2020-09-06 14:04:36 +0200

    added functionality to compile programs

--- Sources/compilerLayer.py
+++ Sources/compilerLayer.py
@@ -4,23 +4,28 @@ from subprocess import run
 
 
 def compileAssembly(input_file: str, output_file: str):
-    system(f"gcc -m64 -c {input_file} -o{output_file} {'-no-pie' if sys() == 'Linux' else ''}")
+    if system(f"gcc -m64 -c {input_file} -o{output_file} {'-no-pie' if sys() == 'Linux' else ''}"):
+        return False
     remove(input_file)
+    return True
 
 
 def linkObjects(input_files: list, output_file: str):
-    system(f"gcc -m64 {' '.join(input_files)} -o{output_file} {'-no-pie' if sys() == 'Linux' else ''}")
+    return not system(f"gcc -m64 {' '.join(input_files)} -o{output_file} {'-no-pie' if sys() == 'Linux' else ''}")
 
 
 def compileJaclang(input_file: str, output_file: str):
+    if input_file.startswith("./"):
+        input_file = input_file[2:]
+
     command_object = run(f"/usr/local/Jac/Binaries/jaclang {input_file} -o{output_file}.s --__dump-imports", shell=True,
                          capture_output=True)
     if command_object.returncode:
         print(command_object.stderr.decode("utf-8"), end='')
-        exit(1)
+        return None
     compileAssembly(f"{output_file}.s", output_file)
     return command_object.stdout.decode("utf-8").split("\n")[:-1]
 
 
-def compileObjectsIntoDylib(input_files: list, output_file: str):
-    system(f"gcc -m64 -shared -dynamiclib {' '.join(input_files)} -o{output_file}")
+def linkObjectsIntoDylib(input_files: list, output_file: str):
+    return not system(f"gcc -m64 -shared -dynamiclib {' '.join(input_files)} -o{output_file}")
--- Sources/dirCompiler.py
+++ Sources/dirCompiler.py
@@ -1,39 +1,96 @@
 from os import path, mkdir, listdir
 from shutil import rmtree
 from platform import system as sys
+from json import load, JSONDecodeError
 
-from compilerLayer import compileJaclang, compileObjectsIntoDylib
+from compilerLayer import compileJaclang, linkObjectsIntoDylib, linkObjects, compileAssembly
+from entryGenerator import generateEntry
 
-object_names = []
+info_json, object_names = {}, []
 
 
 def compileSrcDir(src_dir: str, obj_dir: str):
     for file in listdir(src_dir):
         if path.isfile(src_dir + file) and file.endswith(".jl"):
             object_names.append(f"{obj_dir}{file[:-3]}.o")
-            compileJaclang(f"{src_dir}{file}", f"{obj_dir}{file[:-3]}.o")
+            if not path.isfile(f"{obj_dir}{file[:-3]}.o") or \
+                    path.getmtime(src_dir + file) > path.getmtime(f"{obj_dir}{file[:-3]}.o"):
+                if file == "__entry.jl":
+                    print("File cannot be named \"__entry.jl\"")
+                    return False
+                if compileJaclang(src_dir + file, f"{obj_dir}{file[:-3]}.o") is None:
+                    return False
         elif path.isdir(src_dir + file):
             mkdir(obj_dir + file)
-            compileSrcDir(src_dir + file, obj_dir + file)
+            if not compileSrcDir(src_dir + file, obj_dir + file):
+                return False
+    return True
 
 
 def compileLibrary(dir_name: str):
     if path.isdir(f"{dir_name}Objects"):
         rmtree(f"{dir_name}Objects")
     mkdir(f"{dir_name}Objects")
-    compileSrcDir(f"{dir_name}Sources/", f"{dir_name}Objects/")
+    if not compileSrcDir(f"{dir_name}Sources/", f"{dir_name}Objects/"):
+        print("Compilation failed!")
+        return
     if object_names:
-        compileObjectsIntoDylib(object_names, f"{dir_name}/lib.{'so' if sys() == 'Linux' else 'dylib'}")
+        if not linkObjectsIntoDylib(object_names, f"{dir_name}/lib.{'so' if sys() == 'Linux' else 'dylib'}"):
+            print("Compilation failed!")
+            return
     rmtree(f"{dir_name}Objects/")
+
+
+def compileProgram(dir_name: str, exec_name: str, files_to_execute: list):
+    if not path.isdir(f"{dir_name}Objects"):
+        mkdir(f"{dir_name}Objects")
+
+    if not compileSrcDir(f"{dir_name}Sources/", f"{dir_name}Objects/"):
+        print("Compilation failed!")
+        return
+
+    with open("Objects/__entry.s", "w") as entry_file:
+        entry_file.write(generateEntry(files_to_execute))
+    compileAssembly("Objects/__entry.s", "Objects/__entry.o")
+    object_names.append("Objects/__entry.o")
+
+    if not linkObjects(object_names, exec_name):
+        print("Compilation failed!")
+        return
 
 
 def compileDir(dir_name: str):
     if not dir_name[-1] == "/":
         dir_name += "/"
-    if path.isfile(f"{dir_name}Info.json") and path.isdir(f"{dir_name}Sources"):
-        compileLibrary(dir_name)
+    if path.isfile(f"{dir_name}Info.json"):
+        if not path.isdir("Sources"):
+            print(f"Warning: skipping {dir_name} as it is not valid.")
+            return
+        with open(f"{dir_name}Info.json") as file:
+            try:
+                global info_json
+                info_json = load(file)
+            except JSONDecodeError:
+                print(f"Warning: skipping {dir_name} as it does not have a valid json file.")
+                return
+        if "Type" in info_json:
+            if info_json["Type"] == "Package" or info_json["Type"] == "Dependency":
+                compileLibrary(dir_name)
+            elif info_json["Type"] == "Program":
+                if "ExecName" in info_json and "StartFiles" in info_json:
+                    for file in info_json["StartFiles"]:
+                        if not path.isfile(file):
+                            print(f"Starter file {file} does not exist!")
+                            return
+                    compileProgram(dir_name, info_json["ExecName"], info_json["StartFiles"])
+                else:
+                    print(f"Warning: skipping {dir_name} as it does not have a valid info file.")
+            else:
+                print(f"Warning: skipping {dir_name} as it is not recognized as anything.")
+        else:
+            print(f"Warning: skipping {dir_name} as it does not have a valid info file.")
     else:
-        print(f"Warning: skipping {dir_name} as it is not recognized as anything or has already been compiled.")
+        print(f"Warning: skipping {dir_name} as it is not recognized as anything.")
 
 
 def compileDirs(dirs_to_compile: list):
